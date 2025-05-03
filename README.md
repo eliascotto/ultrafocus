61dd4e9a3b6871df1cac14c96eac7acd68e21e4a
Elia Scotto <8095533+eliascotto@users.noreply.github.com>
2025-05-04 09:41:59 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ultrafocus
+
+> Hide Everything Everywhere All at Once
+
+A plugin to turn Obsidian into the ultimate platform for focused writing.
+
+**What it does**: hides all elements like tabs, status, ribbon, sidebars, etc. All with a single toggle command, no buttons.
+
+![demo.gif](demo.gif)
+
+## Settings
+
+Choose what elements you'd like to keep visible. By default, everything will be hidden.
+
+## Alternatives
+
+- [**Hider**](https://github.com/kepano/obsidian-hider/tree/master): Chose what to hide one by one.
+- [**Focus Mode**](https://github.com/ryanpcmcquen/obsidian-focus-mode): Toggle focus with a sidebar button but doesn't hide elements.
---
+++ main.ts
@@ -0,0 +1,162 @@
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+interface UltrafocusSettings {
+	hideRibbon: boolean;
+	hideStatus: boolean;
+	hideTabs: boolean;
+	hideSidebarLeft: boolean;
+	hideSidebarRight: boolean;
+}
+
+const DEFAULT_SETTINGS: UltrafocusSettings = {
+	hideRibbon: true,
+	hideStatus: true,
+	hideTabs: true,
+	hideSidebarLeft: true,
+	hideSidebarRight: true,
+}
+
+export default class Ultrafocus extends Plugin {
+	settings: UltrafocusSettings;
+
+	active: boolean = false;
+	leftSidebarCollapsed: boolean;
+	rightSidebarCollapsed: boolean;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.addSettingTab(new UltrafocusSettingsTab(this.app, this));
+
+		this.addCommand({
+			id: 'toggle-ultrafocus',
+			name: 'Toggle Ultrafocus',
+			callback: () => {
+				this.active = !this.active;
+				this.updateStyle();
+			},
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'space' }]
+		});
+	}
+
+	onunload() {
+		console.log('Unloading Ultrafocus plugin');
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	storeSidebarState() {
+		this.leftSidebarCollapsed = this.app.workspace.leftSplit.collapsed;
+		this.rightSidebarCollapsed = this.app.workspace.rightSplit.collapsed;
+	}
+
+	collapseSidebar() {
+		if (this.settings.hideSidebarLeft) {
+			this.app.workspace.leftSplit.collapse();
+		}
+
+		if (this.settings.hideSidebarRight) {
+			this.app.workspace.rightSplit.collapse();
+		}
+	}
+
+	restoreSidebar() {
+		if (!this.leftSidebarCollapsed) {
+			this.app.workspace.leftSplit.expand();
+		}
+
+		if (!this.rightSidebarCollapsed) {
+			this.app.workspace.rightSplit.expand();
+		}
+	}
+
+	updateStyle() {
+		if (this.settings.hideRibbon) {
+			document.body.classList.toggle('ultrafocus-ribbon', this.active);
+		}
+
+		if (this.settings.hideStatus) {
+			document.body.classList.toggle('ultrafocus-status', this.active);
+		}
+
+		if (this.settings.hideTabs) {
+			document.body.classList.toggle('ultrafocus-tabs', this.active);
+		}
+
+		if (this.active) {
+			this.storeSidebarState();
+			this.collapseSidebar();
+		} else {
+			this.restoreSidebar();
+		}
+	}
+};
+
+class UltrafocusSettingsTab extends PluginSettingTab {
+	plugin: Ultrafocus;
+	constructor(app: App, plugin: Ultrafocus) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Hide app ribbon')
+			.setDesc('Hides the Obsidian menu. Warning: to open Settings you will need use the hotkey (default is CMD + ,)')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.hideTabs)
+				.onChange((value) => {
+					this.plugin.settings.hideTabs = value;
+					this.plugin.saveData(this.plugin.settings);
+				})
+			);
+
+		new Setting(containerEl)
+			.setName('Hide tab bar')
+			.setDesc('Hides the tab container at the top of the window.')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.hideTabs)
+				.onChange((value) => {
+					this.plugin.settings.hideTabs = value;
+					this.plugin.saveData(this.plugin.settings);
+				})
+			);
+
+		new Setting(containerEl)
+			.setName('Hide status bar')
+			.setDesc('Hides word count, character count and backlink count.')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.hideStatus)
+				.onChange((value) => {
+					this.plugin.settings.hideStatus = value;
+					this.plugin.saveData(this.plugin.settings);
+				})
+			);
+
+		new Setting(containerEl)
+			.setName('Hide left sidebar')
+			.setDesc('Collapse left sidebar when active.')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.hideSidebarLeft)
+				.onChange((value) => {
+					this.plugin.settings.hideSidebarLeft = value;
+					this.plugin.saveData(this.plugin.settings);
+				})
+			);
+
+		new Setting(containerEl)
+			.setName('Hide right sidebar')
+			.setDesc('Collapse right sidebar when active.')
+			.addToggle(toggle => toggle.setValue(this.plugin.settings.hideSidebarRight)
+				.onChange((value) => {
+					this.plugin.settings.hideSidebarRight = value;
+					this.plugin.saveData(this.plugin.settings);
+				})
+			);
+	}
+};
